c3bb069964fb38fb3311f263701233f1a2c45d8f
hbrill <hbrillant027@gmail.com>
2022-09-27 20:42:44 -0400

    Added comments for easier understanding when studying

--- LC26/Solution.java
+++ LC26/Solution.java
@@ -7,7 +7,7 @@ class Solution {
             /* We start looping at second index. If element at index is not equals to the element at the non_dup index,
             we move this pointer one up and replace the value at this index with this value. 
             Keep in mind this works because we have input in the format [0,0,1,2,2,3,4,4,5], and since repeated elements
-            are consequents, we can just move the pointer up and replace any instance of the element after the second one
+            are consequents, we can just move the pointer up and replace any instance of the element after the first one
             with the non-repeated elements. */
             
             if(nums[i] != nums[non_dup]){
