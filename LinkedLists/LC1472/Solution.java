93b1c4f2ecd2582d018c6878939ec73543d11242
hbrill <hbrillant027@gmail.com>
2022-12-10 21:45:10 -0500

    adding comments to LC1472 BrowserHistory implementation

--- LinkedLists/LC1472/Solution.java
+++ LinkedLists/LC1472/Solution.java
@@ -1,3 +1,4 @@
+// A LL Node represents a single page
 class Page {
     Page prev, next;
     String url;
@@ -12,6 +13,7 @@ class BrowserHistory {
     int backSteps, forwardSteps = 0;
     public BrowserHistory(String homepage) {
         Page newPage = new Page(homepage);
+        // when you open a page, that is both the first and last page in your history
         first = newPage;
         last = first;
         current = last;
@@ -19,32 +21,36 @@ class BrowserHistory {
     
     public void visit(String url) {
         Page newNode = new Page(url);
+        // if we visit a page from another page that is not the last, reset all forward steps
         if(current != last)
             forwardSteps = 0;
         
+        // new page's previous page will be our current page
         newNode.prev = current;
-        current.next = newNode;
-        last = newNode;
-        current = last;
-        backSteps++;
+        current.next = newNode; // current page's next page will be the new one
+        last = newNode; // new page becomes the latest visited one
+        current = last; // current page also becomes the latest visited one 
+        backSteps++; // we can now go back an extra page
         
     }
     
     public String back(int steps) {
         if(backSteps == 0)
-            return first.url;
+            return first.url; // if there are no pages to go back to, then return the first/current page
         else if(backSteps <=  steps){
-            forwardSteps += backSteps;
-            backSteps -= backSteps;
-            current = first;
+            // if number of steps to go back is greater than number of pages visited previously, go back as many pages back as possible
+            forwardSteps += backSteps; // since we're going all the way back, the number of forward steps is added the number of back steps we took
+            backSteps -= backSteps; // backsteps taken are subtracted from the variable, or become 0
+            current = first; // our current page is the initia; one
             return first.url;
         }else{
+            // else, simply go back as many steps as requested
             Page ptr = current;
             int i = 0;
             while(i < steps){
-                ptr = ptr.prev;
-                backSteps--;
-                forwardSteps++;
+                ptr = ptr.prev; // point to the current's previous page
+                backSteps--; // as we go back, we lose backsteps to take
+                forwardSteps++; // and gain forward steps
                 i++;
             }
             current = ptr;
@@ -52,6 +58,7 @@ class BrowserHistory {
         }
     }
     
+    // similar as above, but moving forward instead
     public String forward(int steps) {
         if(forwardSteps == 0)
             return current.url;
